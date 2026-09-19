b08269857942a470d1242398876d8b8859bcbca0
Eduardo Hernandes <eduardo.hernandes@hotmail.com>
2026-09-19 14:52:51 -0300

    Update SQL dump to include user table structure and sample data for library system

--- database/biblioteca_2024-10-10.sql
+++ database/biblioteca_2024-10-10.sql
@@ -1,169 +1,197 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 10-Out-2024 às 18:48
--- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.1.12
+  -- phpMyAdmin SQL Dump
+  -- version 5.2.0
+  -- https://www.phpmyadmin.net/
+  --
+  -- Host: 127.0.0.1
+  -- Tempo de geração: 10-Out-2024 às 18:48
+  -- Versão do servidor: 10.4.27-MariaDB
+  -- versão do PHP: 8.1.12
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+  SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+  START TRANSACTION;
+  SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+  /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+  /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+  /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+  /*!40101 SET NAMES utf8mb4 */;
 
---
--- Banco de dados: `db_biblioteca`
---
-CREATE DATABASE IF NOT EXISTS `db_biblioteca` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `db_biblioteca`;
+  --
+  -- Banco de dados: `db_biblioteca`
+  --
+  CREATE DATABASE IF NOT EXISTS `db_biblioteca` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+  USE `db_biblioteca`;
 
--- --------------------------------------------------------
+  -- --------------------------------------------------------
 
---
--- Estrutura da tabela `emprestimos`
---
+  --
+  -- Estrutura da tabela `usuarios`
+  --
 
-CREATE TABLE `emprestimos` (
-  `CodEmprestimo` int(11) NOT NULL,
-  `Data_Emprestimo` date DEFAULT NULL,
-  `Data_Devolucao` date DEFAULT NULL,
-  `CodLivro` int(11) DEFAULT NULL,
-  `CodLeitor` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  CREATE TABLE `usuarios` (
+    `CodUsuario` int(11) NOT NULL,
+    `Nome` varchar(255) NOT NULL,
+    `Email` varchar(255) NOT NULL,
+    `Senha` varchar(255) NOT NULL,
+    `Tipo` enum('admin','bibliotecario','usuario') NOT NULL DEFAULT 'usuario',
+    `Status` enum('ativo','inativo') NOT NULL DEFAULT 'ativo',
+    `DataCadastro` datetime NOT NULL DEFAULT current_timestamp()
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Extraindo dados da tabela `emprestimos`
---
+  --
+  -- Índices para tabela `usuarios`
+  --
+  ALTER TABLE `usuarios`
+    ADD PRIMARY KEY (`CodUsuario`),
+    ADD UNIQUE KEY `Email` (`Email`);
 
-INSERT INTO `emprestimos` (`CodEmprestimo`, `Data_Emprestimo`, `Data_Devolucao`, `CodLivro`, `CodLeitor`) VALUES
-(1, '2024-09-02', '2024-09-09', 2, 1),
-(2, '2024-09-10', '2024-09-17', 5, 2),
-(3, '2024-09-10', '2024-09-17', 4, 2),
-(4, '2024-09-18', '2024-09-25', 3, 3);
+  --
+  -- AUTO_INCREMENT de tabela `usuarios`
+  --
+  ALTER TABLE `usuarios`
+    MODIFY `CodUsuario` int(11) NOT NULL AUTO_INCREMENT;
 
--- --------------------------------------------------------
+  -- --------------------------------------------------------
 
---
--- Estrutura da tabela `leitores`
---
+  --
+  -- Estrutura da tabela `emprestimos`
+  --
 
-CREATE TABLE `leitores` (
-  `CodLeitor` int(11) NOT NULL,
-  `Nome` varchar(255) DEFAULT NULL,
-  `DtNasc` date DEFAULT NULL,
-  `Celular` varchar(255) DEFAULT NULL,
-  `Email` varchar(255) DEFAULT NULL,
-  `RA` int(11) DEFAULT NULL,
-  `Endereco` varchar(255) DEFAULT NULL,
-  `NumEnd` varchar(255) DEFAULT NULL,
-  `Bairro` varchar(255) DEFAULT NULL,
-  `CidadeUF` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  CREATE TABLE `emprestimos` (
+    `CodEmprestimo` int(11) NOT NULL,
+    `Data_Emprestimo` date DEFAULT NULL,
+    `Data_Devolucao` date DEFAULT NULL,
+    `CodLivro` int(11) DEFAULT NULL,
+    `CodLeitor` int(11) DEFAULT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Extraindo dados da tabela `leitores`
---
+  --
+  -- Extraindo dados da tabela `emprestimos`
+  --
 
-INSERT INTO `leitores` (`CodLeitor`, `Nome`, `DtNasc`, `Celular`, `Email`, `RA`, `Endereco`, `NumEnd`, `Bairro`, `CidadeUF`) VALUES
-(1, 'João Silva Sauro', '2001-04-01', '(11)98788-2311', 'joao@hotmail.com', 631113101, 'Rua Boa Esperança', '201', 'Janga', 'Paulista/PE'),
-(2, 'Maria Silva Nascimento', '1984-03-02', '(11)98788-2311', 'carlos.almeida@gmail.com', 631113102, 'Rua da Vovó', '101', 'MAranguape I', 'Paulista/PE'),
-(3, 'Carlos Andrade de Almeida', '1984-03-02', '21', 'carlos.almeida@gmail.com', 456, 'Rua das Amelias Amarelas', '478', 'Avivado', 'Paulista/PE'),
-(4, 'Eduardo Hernandes', '1984-03-02', '(81)', 'edu@edu,br', 78771, 'Rua das Camelias', '11', 'Janga', 'Paulista/PE'),
-(5, 'Jose Hernandes', '2004-05-02', '(81)', 'edu@hot,br', 78775, 'Rua das Salamndras', '12', 'Janga', 'Paulista/PE'),
-(7, 'Teste Nome', '2001-10-20', '(81) 99999-9999', 'teste@teste.com', 1111111, 'Rua de Teste', '99', 'Janga', 'Paulista/PE');
+  INSERT INTO `emprestimos` (`CodEmprestimo`, `Data_Emprestimo`, `Data_Devolucao`, `CodLivro`, `CodLeitor`) VALUES
+  (1, '2024-09-02', '2024-09-09', 2, 1),
+  (2, '2024-09-10', '2024-09-17', 5, 2),
+  (3, '2024-09-10', '2024-09-17', 4, 2),
+  (4, '2024-09-18', '2024-09-25', 3, 3);
 
--- --------------------------------------------------------
+  -- --------------------------------------------------------
 
---
--- Estrutura da tabela `livros`
---
+  --
+  -- Estrutura da tabela `leitores`
+  --
 
-CREATE TABLE `livros` (
-  `CodLivro` int(11) NOT NULL,
-  `Titulo` varchar(255) DEFAULT NULL,
-  `Autor` varchar(255) DEFAULT NULL,
-  `Editora` varchar(255) DEFAULT NULL,
-  `Sinopse` varchar(255) DEFAULT NULL,
-  `AnoPublicacao` int(11) DEFAULT NULL,
-  `Genero` varchar(255) DEFAULT NULL,
-  `Paginas` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  CREATE TABLE `leitores` (
+    `CodLeitor` int(11) NOT NULL,
+    `Nome` varchar(255) DEFAULT NULL,
+    `DtNasc` date DEFAULT NULL,
+    `Celular` varchar(255) DEFAULT NULL,
+    `Email` varchar(255) DEFAULT NULL,
+    `RA` int(11) DEFAULT NULL,
+    `Endereco` varchar(255) DEFAULT NULL,
+    `NumEnd` varchar(255) DEFAULT NULL,
+    `Bairro` varchar(255) DEFAULT NULL,
+    `CidadeUF` varchar(255) DEFAULT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Extraindo dados da tabela `livros`
---
+  --
+  -- Extraindo dados da tabela `leitores`
+  --
 
-INSERT INTO `livros` (`CodLivro`, `Titulo`, `Autor`, `Editora`, `Sinopse`, `AnoPublicacao`, `Genero`, `Paginas`) VALUES
-(1, 'O Senhor dos Anéis: A Sociedade do Anel', 'J.R.R. Tolkien', 'HarperCollins', 'Primeiro livro da trilogia O Senhor dos Anéis, onde Frodo e seus companheiros tentam destruir o Um Anel.', 1954, 'Fantasia', 576),
-(2, '1984', 'George Orwell', 'Companhia das Letras', 'Romance distópico que retrata uma sociedade totalitária sob vigilância constante.', 1949, 'Ficção científica', 416),
-(3, 'Dom Casmurro', 'Machado de Assis', 'Principis', 'A história de amor e ciúmes entre Bentinho e Capitu.', 1899, 'Clássico', 256),
-(4, 'O Código Da Vinci', 'Dan Brown', 'Arqueiro', 'Robert Langdon investiga um assassinato e descobre segredos sobre o Santo Graal.', 2003, 'Suspense', 480),
-(5, 'A Revolução dos Bichos', 'George Orwell', 'Companhia das Letras', 'Uma alegoria política sobre uma revolução liderada por animais em uma fazenda.', 1945, 'Sátira', 152);
+  INSERT INTO `leitores` (`CodLeitor`, `Nome`, `DtNasc`, `Celular`, `Email`, `RA`, `Endereco`, `NumEnd`, `Bairro`, `CidadeUF`) VALUES
+(1, 'João Silva Santos', '2001-04-01', '(81) 98788-2311', 'joao.silva@gmail.com', 631113101, 'Rua Boa Esperança', '201', 'Janga', 'Paulista/PE'),
+(2, 'Maria da Silva Nascimento', '1984-03-02', '(81) 98877-4422', 'maria.nascimento@gmail.com', 631113102, 'Rua da Vovó', '101', 'Maranguape I', 'Paulista/PE'),
+(3, 'Carlos Andrade de Almeida', '1990-07-15', '(81) 99654-7788', 'carlos.almeida@gmail.com', 456123, 'Rua das Amélia', '478', 'Abreu e Lima', 'Paulista/PE'),
+(4, 'Eduardo Hernandes', '1988-11-27', '(81) 99123-4567', 'eduardo.hernandes@gmail.com', 78771, 'Rua das Camelias', '11', 'Janga', 'Paulista/PE'),
+(5, 'José Hernandes', '2004-05-02', '(81) 98456-1123', 'jose.hernandes@gmail.com', 78775, 'Rua das Salamandras', '12', 'Janga', 'Paulista/PE'),
 
---
--- Índices para tabelas despejadas
---
+  -- --------------------------------------------------------
 
---
--- Índices para tabela `emprestimos`
---
-ALTER TABLE `emprestimos`
-  ADD PRIMARY KEY (`CodEmprestimo`),
-  ADD KEY `CodLivro` (`CodLivro`),
-  ADD KEY `CodLeitor` (`CodLeitor`);
+  --
+  -- Estrutura da tabela `livros`
+  --
 
---
--- Índices para tabela `leitores`
---
-ALTER TABLE `leitores`
-  ADD PRIMARY KEY (`CodLeitor`);
+  CREATE TABLE `livros` (
+    `CodLivro` int(11) NOT NULL,
+    `Titulo` varchar(255) DEFAULT NULL,
+    `Autor` varchar(255) DEFAULT NULL,
+    `Editora` varchar(255) DEFAULT NULL,
+    `Sinopse` varchar(255) DEFAULT NULL,
+    `AnoPublicacao` int(11) DEFAULT NULL,
+    `Genero` varchar(255) DEFAULT NULL,
+    `Paginas` int(11) DEFAULT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices para tabela `livros`
---
-ALTER TABLE `livros`
-  ADD PRIMARY KEY (`CodLivro`);
+  --
+  -- Extraindo dados da tabela `livros`
+  --
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
+  INSERT INTO `livros` (`CodLivro`, `Titulo`, `Autor`, `Editora`, `Sinopse`, `AnoPublicacao`, `Genero`, `Paginas`) VALUES
+  (1, 'O Senhor dos Anéis: A Sociedade do Anel', 'J.R.R. Tolkien', 'HarperCollins', 'Primeiro livro da trilogia O Senhor dos Anéis, onde Frodo e seus companheiros tentam destruir o Um Anel.', 1954, 'Fantasia', 576),
+  (2, '1984', 'George Orwell', 'Companhia das Letras', 'Romance distópico que retrata uma sociedade totalitária sob vigilância constante.', 1949, 'Ficção científica', 416),
+  (3, 'Dom Casmurro', 'Machado de Assis', 'Principis', 'A história de amor e ciúmes entre Bentinho e Capitu.', 1899, 'Clássico', 256),
+  (4, 'O Código Da Vinci', 'Dan Brown', 'Arqueiro', 'Robert Langdon investiga um assassinato e descobre segredos sobre o Santo Graal.', 2003, 'Suspense', 480),
+  (5, 'A Revolução dos Bichos', 'George Orwell', 'Companhia das Letras', 'Uma alegoria política sobre uma revolução liderada por animais em uma fazenda.', 1945, 'Sátira', 152);
 
---
--- AUTO_INCREMENT de tabela `emprestimos`
---
-ALTER TABLE `emprestimos`
-  MODIFY `CodEmprestimo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  --
+  -- Índices para tabelas despejadas
+  --
 
---
--- AUTO_INCREMENT de tabela `leitores`
---
-ALTER TABLE `leitores`
-  MODIFY `CodLeitor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  --
+  -- Índices para tabela `emprestimos`
+  --
+  ALTER TABLE `emprestimos`
+    ADD PRIMARY KEY (`CodEmprestimo`),
+    ADD KEY `CodLivro` (`CodLivro`),
+    ADD KEY `CodLeitor` (`CodLeitor`);
 
---
--- AUTO_INCREMENT de tabela `livros`
---
-ALTER TABLE `livros`
-  MODIFY `CodLivro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  --
+  -- Índices para tabela `leitores`
+  --
+  ALTER TABLE `leitores`
+    ADD PRIMARY KEY (`CodLeitor`);
 
---
--- Restrições para despejos de tabelas
---
+  --
+  -- Índices para tabela `livros`
+  --
+  ALTER TABLE `livros`
+    ADD PRIMARY KEY (`CodLivro`);
 
---
--- Limitadores para a tabela `emprestimos`
---
-ALTER TABLE `emprestimos`
-  ADD CONSTRAINT `emprestimos_ibfk_1` FOREIGN KEY (`CodLivro`) REFERENCES `livros` (`CodLivro`),
-  ADD CONSTRAINT `emprestimos_ibfk_2` FOREIGN KEY (`CodLeitor`) REFERENCES `leitores` (`CodLeitor`);
-COMMIT;
+  --
+  -- AUTO_INCREMENT de tabelas despejadas
+  --
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  --
+  -- AUTO_INCREMENT de tabela `emprestimos`
+  --
+  ALTER TABLE `emprestimos`
+    MODIFY `CodEmprestimo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+  --
+  -- AUTO_INCREMENT de tabela `leitores`
+  --
+  ALTER TABLE `leitores`
+    MODIFY `CodLeitor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+  --
+  -- AUTO_INCREMENT de tabela `livros`
+  --
+  ALTER TABLE `livros`
+    MODIFY `CodLivro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+  --
+  -- Restrições para despejos de tabelas
+  --
+
+  --
+  -- Limitadores para a tabela `emprestimos`
+  --
+  ALTER TABLE `emprestimos`
+    ADD CONSTRAINT `emprestimos_ibfk_1` FOREIGN KEY (`CodLivro`) REFERENCES `livros` (`CodLivro`),
+    ADD CONSTRAINT `emprestimos_ibfk_2` FOREIGN KEY (`CodLeitor`) REFERENCES `leitores` (`CodLeitor`);
+  COMMIT;
+
+  /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+  /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+  /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
